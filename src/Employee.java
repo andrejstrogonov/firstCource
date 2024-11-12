92d5b036bdf29e75ba32814ddde8bce0d2f7d66d
andrejstrogonov <strogonovandrej@hotmail.com>
2024-11-12 21:13:14 +0300

    Employee.java completed

---
+++ src/Employee.java
@@ -0,0 +1,67 @@
+import java.util.Objects;
+
+public class Employee {
+    private String name;
+    private String surname;
+    private String patronimic;
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    private double salary;
+    private int department;
+    private static int id;
+
+    // Constructor
+    public Employee(String name, String surname, String patronimic,double salary, int department) {
+        this.name = name;
+        this.surname = surname;
+        this.patronimic = patronimic;
+        this.salary = salary;
+        this.department = department;
+        this.id = id;
+        id++;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+    public String getPatronimic() {
+        return patronimic;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+
+    public static int getId() {
+        return id;
+    }
+
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Double.compare(salary, employee.salary) == 0 && department == employee.department && Objects.equals(name, employee.name) && Objects.equals(surname, employee.surname) && Objects.equals(patronimic, employee.patronimic);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, patronimic, salary, department);
+    }
+}
