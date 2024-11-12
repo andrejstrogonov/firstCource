45047876be117172699d0100b3fdd4839b940345
andrejstrogonov <strogonovandrej@hotmail.com>
2024-11-12 21:44:52 +0300

    Corrections in employees

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         Employee employee = new Employee("Ivan", "Ivanov", "Ivanovich", 2000, 1);
-        Employee employee2 = new Employee("Ivan", "Ivanov", "Ivanovich", 2000, 1);
-        Employee employee3 = new Employee("Petr", "Petrov", "Petrovich", 3000, 2);
-        Employee employee4 = new Employee("Petr", "Petrov", "Petrovich", 3000, 2);
-        Employee employee5 = new Employee("Petr", "Petrov", "Petrovich", 3000, 2);
-        Employee employee6 = new Employee("Petr", "Petrov", "Petrovich", 3000, 2);
-        Employee employee7 = new Employee("Petr", "Petrov", "Petrovich", 3000, 2);
-        Employee employee8 = new Employee("Petr", "Petrov", "Petrovich", 3000, 2);
-        Employee employee9 = new Employee("Petr", "Petrov", "Petrovich", 3000, 2);
-        Employee employee10 = new Employee("Petr", "Petrov", "Petrovich", 3000, 2);
+        Employee employee2 = new Employee("Ivan", "Ivanov", "Ivanovich", 2500, 1);
+        Employee employee3 = new Employee("Petr", "Petrov", "Petrovich", 3200, 2);
+        Employee employee4 = new Employee("Semen", "Sidorov", "Petrovich", 3100, 3);
+        Employee employee5 = new Employee("Oleg", "Petrov", "Petrovich", 3200, 2);
+        Employee employee6 = new Employee("Dmitrii", "Elev", "Petrovich", 3300, 4);
+        Employee employee7 = new Employee("Fedor", "Sergeev", "Yanovich", 3400, 2);
+        Employee employee8 = new Employee("Petr", "Petrov", "Lasarevich", 8000, 4);
+        Employee employee9 = new Employee("Mosha", "Israelevich", "Petrovich", 3090, 2);
+        Employee employee10 = new Employee("Petr", "Petrov", "Petrovich", 3000, 5);
 
 
 
