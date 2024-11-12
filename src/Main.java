e3f8acc375114f3837159cf6a80f925c05825a5a
andrejstrogonov <strogonovandrej@hotmail.com>
2024-11-12 21:40:34 +0300

    Add employees

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee employee = new Employee("Ivan", "Ivanov", "Ivanovich", 2000, 1);
+        Employee employee2 = new Employee("Ivan", "Ivanov", "Ivanovich", 2000, 1);
+        Employee employee3 = new Employee("Petr", "Petrov", "Petrovich", 3000, 2);
+        Employee employee4 = new Employee("Petr", "Petrov", "Petrovich", 3000, 2);
+        Employee employee5 = new Employee("Petr", "Petrov", "Petrovich", 3000, 2);
+        Employee employee6 = new Employee("Petr", "Petrov", "Petrovich", 3000, 2);
+        Employee employee7 = new Employee("Petr", "Petrov", "Petrovich", 3000, 2);
+        Employee employee8 = new Employee("Petr", "Petrov", "Petrovich", 3000, 2);
+        Employee employee9 = new Employee("Petr", "Petrov", "Petrovich", 3000, 2);
+        Employee employee10 = new Employee("Petr", "Petrov", "Petrovich", 3000, 2);
+
+
+
     }
 }
